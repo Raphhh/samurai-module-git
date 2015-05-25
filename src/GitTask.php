8b9703799ed3edf739fd32595def8cea1b7bc75f
Raphaël Lefebvre <raphael@raphaellefebvre.be>
2015-05-25 20:36:36 +0200

    implement init git logic

--- src/GitTask.php
+++ src/GitTask.php
@@ -1,6 +1,7 @@
 <?php
 namespace Samurai\Git;
 
+use Samurai\Task\ITask;
 use Samurai\Task\Task;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -15,11 +16,19 @@ class GitTask extends Task
     /**
      * @param InputInterface $input
      * @param OutputInterface $output
-     * @return bool
+     * @return int
      */
     public function execute(InputInterface $input, OutputInterface $output)
     {
         $output->writeln('<info>Initializing Git</info>');
-        return true;
+        try{
+            $this->getService('git')->status();
+            $output->writeln('<info>Repository already initialized</info>');
+            return ITask::NON_BLOCKING_ERROR_CODE;
+        }catch(\Exception $e){
+            $dirPath = $this->getService('project')->getDirectoryPath() ?: getcwd();
+            $this->getService('git')->init($dirPath);
+            return ITask::NO_ERROR_CODE;
+        }
     }
 }
--- tests/GitTaskTest.php
+++ tests/GitTaskTest.php
@@ -2,6 +2,8 @@
 namespace Samurai\Git;
 
 use Pimple\Container;
+use Samurai\Project\Project;
+use Samurai\Task\ITask;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\BufferedOutput;
 
@@ -17,12 +19,34 @@ class GitTaskTest extends \PHPUnit_Framework_TestCase
      */
     public function testExecute()
     {
+        $git = $this->getMockBuilder('PHPGit\Git')->disableOriginalConstructor()->getMock();
+
+        $git->expects($this->at(0))
+            ->method('__call')
+            ->with('status', [])
+            ->will($this->throwException(new \Exception()));
+
+        $git->expects($this->at(1))
+            ->method('__call', ['path' => 'dir/path'])
+            ->with('init');
+
         $input = new ArrayInput([]);
         $output = new BufferedOutput();
 
         $services = new Container();
+
+        $services['project'] = function(){
+            $project = new Project();
+            $project->setDirectoryPath('dir/path');
+            return $project;
+        };
+
+        $services['git'] = function() use($git){
+            return $git;
+        };
+
         $task = new GitTask($services);
-        $this->assertTrue($task->execute($input, $output));
+        $this->assertSame(ITask::NO_ERROR_CODE, $task->execute($input, $output));
         $this->assertSame("Initializing Git\n", $output->fetch());
     }
 }
